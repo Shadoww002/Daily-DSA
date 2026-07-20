c774af302042d5618f8bfb6f35cb5e3c5e17d090
Sanjayyyy <sanjudhaker44@gmail.com>
2026-07-21 00:30:13 +0530

    Time: 0 ms (100%), Space: 14.6 MB (50.28%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    void dfs(int row , int col , vector<vector<char>>&grid , vector<vector<int>> &vis){
+        vis[row][col] = 1;
+
+        int dr[4] = {1 , 0 , -1 , 0};
+        int dc[4] = {0 , 1 , 0 , -1};
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i = 0 ; i<4 ; i++){
+            int nrow = row + dr[i];
+            int ncol = col + dc[i];
+
+            if(nrow >= 0 && nrow < m && ncol >=0 && ncol < n &&
+                vis[nrow][ncol] == 0 && grid[nrow][ncol] == 'O'){
+                    dfs(nrow , ncol , grid , vis);
+                }
+        }
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> vis(m , vector<int>(n , 0));
+
+        for(int i = 0 ; i < m ; i++){
+            if(board[i][0] == 'O' ) vis[i][0] = 1 ;
+            if(board[i][n-1] == 'O') vis[i][n-1] = 1 ;
+        }
+
+        for(int j = 0 ; j < n ; j++){
+            if(board[0][j] == 'O' ) vis[0][j] = 1 ;
+            if(board[m-1][j] == 'O' ) vis[m-1][j] = 1 ;
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j <  n ; j++){
+                if(vis[i][j] == 1 && board[i][j] == 'O'){
+                    dfs(i , j , board , vis);
+                }
+            }
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(vis[i][j] == 0 && board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
