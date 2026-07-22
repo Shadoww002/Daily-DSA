8839ede764de4be81dbc72dd31ead4215f62e4d1
Sanjayyyy <sanjudhaker44@gmail.com>
2026-07-22 23:41:21 +0530

    Time: 8 ms (34.36%), Space: 20.4 MB (11.19%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private :
+    bool dfs(int node , vector<vector<int>>&adj , vector<int>&vis , vector<int> &pathvis){
+        vis[node] = 1 ;
+        pathvis[node] = 1 ;
+
+        for(auto it : adj[node]){
+            if(vis[it] == 0){
+                if(dfs(it , adj , vis , pathvis) == true) return true ;
+            }
+
+            else if(pathvis[it]) return true;
+        }
+
+        pathvis[node] = 0 ;
+        return false ;
+    }
+
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses ;
+        vector<vector<int>> adj(n);
+
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            adj[u].push_back(v);
+        }
+
+        vector<int> vis(n , 0);
+        vector<int> pathvis(n , 0);
+
+        for(int i = 0 ; i < n ; i++){
+            if(vis[i] == 0){
+                if(dfs(i , adj , vis , pathvis) == true) return false ;
+            }
+        }
+
+        return true ;
+    }
+};
