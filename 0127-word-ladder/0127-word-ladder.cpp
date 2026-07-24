8326e92bb2f4a3fca346a601e53924a5a19edeb9
Sanjayyyy <sanjudhaker44@gmail.com>
2026-07-24 22:16:12 +0530

    Time: 80 ms (40.31%), Space: 22.9 MB (19.41%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+
+        unordered_map<string , int> mp ;
+        for(int i = 0 ; i<n ; i++){
+            mp[wordList[i]] = i ;
+        }
+
+        vector<int> vis(n , 0);
+        queue<pair<string , int>> q ;
+        q.push({beginWord , 1});
+
+        if(mp.find(beginWord) != mp.end()){
+            vis[mp[beginWord]] = 1 ;
+        }
+
+        while(!q.empty()){
+            string word = q.front().first ;
+            int step = q.front().second;
+            q.pop();
+            
+            if(word == endWord) return step;
+
+            for(int i = 0 ; i < word.size() ; i++){
+                char original = word[i];
+                for(char ch = 'a' ; ch <= 'z' ; ch++){
+                    word[i] = ch;
+                    if(mp.find(word) != mp.end() && vis[mp[word]] == 0){
+                        vis[mp[word]] = 1 ;
+                        q.push({word , step + 1});
+                    }
+                }
+                word[i] = original ;
+            }
+        }
+        
+        return 0 ;
+
+    }
+};
