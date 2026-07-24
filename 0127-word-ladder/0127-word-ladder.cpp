07ea3b5e4d665e7dbcea9681d6f5c22bfc751011
Sanjayyyy <sanjudhaker44@gmail.com>
2026-07-24 23:25:47 +0530

    Time: 79 ms (40.96%), Space: 22.6 MB (20.8%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
         int n = wordList.size();
 
-        unordered_map<string , int> mp ;
-        for(int i = 0 ; i<n ; i++){
-            mp[wordList[i]] = i ;
+        unordered_map<string, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[wordList[i]] = i;
         }
 
-        vector<int> vis(n , 0);
-        queue<pair<string , int>> q ;
-        q.push({beginWord , 1});
+        vector<int> vis(n, 0);
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
 
-        if(mp.find(beginWord) != mp.end()){
-            vis[mp[beginWord]] = 1 ;
+        if (mp.find(beginWord) != mp.end()) {
+            vis[mp[beginWord]] = 1;
         }
 
-        while(!q.empty()){
-            string word = q.front().first ;
+        while (!q.empty()) {
+            string word = q.front().first;
             int step = q.front().second;
             q.pop();
-            
-            if(word == endWord) return step;
 
-            for(int i = 0 ; i < word.size() ; i++){
+            if (word == endWord)
+                return step;
+
+            for (int i = 0; i < word.size(); i++) {
                 char original = word[i];
-                for(char ch = 'a' ; ch <= 'z' ; ch++){
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (ch == original)
+                        continue;
                     word[i] = ch;
-                    if(mp.find(word) != mp.end() && vis[mp[word]] == 0){
-                        vis[mp[word]] = 1 ;
-                        q.push({word , step + 1});
+                    if (mp.find(word) != mp.end() && vis[mp[word]] == 0) {
+                        vis[mp[word]] = 1;
+                        q.push({word, step + 1});
                     }
                 }
-                word[i] = original ;
+                word[i] = original;
             }
         }
-        
-        return 0 ;
 
+        return 0;
     }
 };
