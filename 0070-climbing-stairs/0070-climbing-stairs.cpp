01921c3a9b0a618b2bd33633739d53d9540e2c2a
Sanjayyyy <sanjudhaker44@gmail.com>
2026-08-04 02:39:11 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    vector<int> dp;
+    int ways(int n){
+        if(n < 0) return 0 ;
+        if(n == 0) return 1;
+        
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = ways(n-1) + ways(n-2);
+        }
+public:
+    int climbStairs(int n) {
+        dp.assign(n+1 , -1);
+        return ways(n);
+    }
+};
