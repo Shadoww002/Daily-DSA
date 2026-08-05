2a0e8afece87dc070dd974bf9b74bb1412dc9a26
Sanjayyyy <sanjudhaker44@gmail.com>
2026-08-05 16:56:06 +0530

    Time: 478 ms (21.04%), Space: 366.1 MB (18.62%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -23,7 +23,7 @@ public:
             }
         }
 
-        vector<int> ans;
+        
         for (auto vec : invocations) {
             int u = vec[0];
             int v = vec[1];
@@ -34,7 +34,7 @@ public:
                 return ans;
             }
         }
-
+        vector<int> ans;
         for (int i = 0; i < n; i++) {
             if (vis[i] == 0) {
                 ans.push_back(i);
