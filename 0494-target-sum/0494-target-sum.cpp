e7e54cf9c4825f0758f11e4ef9201a8726a65ebe
Sanjayyyy <sanjudhaker44@gmail.com>
2026-08-06 23:18:34 +0530

    Time: 621 ms (19.91%), Space: 20 MB (32.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,33 @@
 class Solution {
-    // vector<vector<int>> dp ;
-    int Subset(int n , int sum , vector<int> &nums){
-        if(n == 0){
-            if(sum == 0) return 1 ;
-            return 0 ;
+    vector<vector<int>> dp;
+    int Subset(int n, int sum, vector<int>& nums) {
+        if (n == 0) {
+            if (sum == 0)
+                return 1;
+            return 0;
         }
-        // if(dp[n][sum] != -1) return dp[n][sum];
-        return Subset(n-1 , sum + nums[n-1] , nums) 
-                            + Subset(n-1 , sum - nums[n-1] , nums);
+        if (dp[n][sum] != -1)
+            return dp[n][sum];
 
+        if (nums[n - 1] <= sum) {
+            return Subset(n - 1, sum - nums[n - 1], nums) +
+                   Subset(n - 1, sum, nums);
+        } else {
+            return Subset(n - 1, sum , nums);
+        }
     }
+
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
 
-        // dp.assign(n+1 , vector<int>(target + 1 , -1));
+        int total = 0 ;
+        for(int x : nums) total += x ;
+        dp.assign(n + 1, vector<int>(total + 1, -1));
+        target = abs(target);
+        int val = (total + target)/2 ;
 
-        return Subset(n , target , nums);
+        if((total + target) % 2 != 0 || total < target) return 0 ;
+        return Subset(n, val, nums);
     }
 };
