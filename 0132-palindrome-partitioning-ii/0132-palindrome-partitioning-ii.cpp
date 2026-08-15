ad7df103819347b21631fe4d450a5891e2ccc250
Sanjayyyy <sanjudhaker44@gmail.com>
2026-08-15 20:49:22 +0530

    Time: 1533 ms (29.29%), Space: 63.1 MB (6.57%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<vector<int>> dp;
+    bool isPal(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+    int solve(int i, int n, string& s) {
+        if (i == n)
+            return 0;
+        
+        if(dp[i][n] != -1) return dp[i][n];
+        int ans = INT_MAX ;
+        for(int j = i ; j < n ; j ++){
+            if(isPal(i , j , s)){
+                int cost = 1 + solve(j + 1 , n , s);
+                ans = min(cost , ans);
+            }
+        }
+        return dp[i][n] = ans ;
+        
+    }
+
+public:
+    int minCut(string s) {
+        int n = s.size();
+        dp.assign(n + 1, vector<int>(n + 1, -1));
+        return solve(0, s.size() , s)-1;
+    }
+};
