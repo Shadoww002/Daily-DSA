1b8421904835d6bae6f1db1677670fbad81ea932
Sanjayyyy <sanjudhaker44@gmail.com>
2026-08-15 20:52:07 +0530

    Time: 91 ms (57.14%), Space: 115.9 MB (6.57%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,32 +1,51 @@
 class Solution {
     vector<vector<int>> dp;
-    bool isPal(int i, int j, string& s) {
-        while (i < j) {
-            if (s[i++] != s[j--])
-                return false;
-        }
-        return true;
-    }
+    vector<vector<int>> pal;
+
     int solve(int i, int n, string& s) {
+
         if (i == n)
             return 0;
-        
-        if(dp[i][n] != -1) return dp[i][n];
-        int ans = INT_MAX ;
-        for(int j = i ; j < n ; j ++){
-            if(isPal(i , j , s)){
-                int cost = 1 + solve(j + 1 , n , s);
-                ans = min(cost , ans);
+
+        if (dp[i][n] != -1)
+            return dp[i][n];
+
+        int ans = INT_MAX;
+
+        for (int j = i; j < n; j++) {
+
+            if (pal[i][j]) {
+
+                int cost = 1 + solve(j + 1, n, s);
+
+                ans = min(ans, cost);
             }
         }
-        return dp[i][n] = ans ;
-        
+
+        return dp[i][n] = ans;
     }
 
 public:
     int minCut(string s) {
+
         int n = s.size();
+
         dp.assign(n + 1, vector<int>(n + 1, -1));
-        return solve(0, s.size() , s)-1;
+        pal.assign(n, vector<int>(n, 0));
+
+        // Precompute palindromes
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = i; j < n; j++) {
+
+                if (s[i] == s[j] &&
+                    (j - i <= 1 || pal[i + 1][j - 1])) {
+
+                    pal[i][j] = 1;
+                }
+            }
+        }
+
+        return solve(0, n, s) - 1;
     }
 };
